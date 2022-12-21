9f1bcc38a7582e5965a802e41b7e633affa26e8a
Vishal <vishalsingh6475@gmail.com>
2022-12-21 21:21:40 +1100

    Max product subarray

---
+++ 152 Maximum Product Subarray.py
@@ -0,0 +1,20 @@
+
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if len(nums) == 0: return nums[0]
+        
+        max_product = 0
+        
+        return max_product
+        
+
+
+sol = Solution()
+
+print(sol.maxProduct([3,30,34,5,9]))
+print(sol.maxProduct([2,3,-2,4]))
