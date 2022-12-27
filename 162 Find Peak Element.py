37c44c372adcfc93977aba0faef3dbf1966e2a36
Vishal <vishalsingh6475@gmail.com>
2022-12-28 03:10:48 +1100

    Find peak element completed

--- 162 Find Peak Element.py
+++ 162 Find Peak Element.py
@@ -1,6 +1,27 @@
+
 class Solution(object):
     def findPeakElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
+        
+        if len(nums) == 1:
+            return 0
+        
+        low = 0
+        high = len(nums)-1
+        
+        while low < high:
+            mid = (low + high) // 2
+            
+            if nums[mid] > nums[mid+1]:
+                high = mid
+            else:
+                low = mid+1
+            
+        return low
+        
+sol = Solution()
+
+print(sol.findPeakElement([1,2,1,3,5,6,4]))
