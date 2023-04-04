90c3df2ee3b8d5dddbc08cab11fafd1e7a7d58d1
Vishal <vishalsingh6475@gmail.com>
2023-04-04 23:25:23 +1000

    Search 2d matrix done

--- 240 Search a 2D Matrix II.cpp
+++ 240 Search a 2D Matrix II.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        
+        int m = matrix.size();
+        int i = 0, j = matrix[0].size()-1;
+
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                matrix.clear();
+                return true;
+            }
+
+            if (matrix[i][j] < target) i++;
+            else j--;
+        }
+
+        matrix.clear();
+        return false;        
     }
 };
