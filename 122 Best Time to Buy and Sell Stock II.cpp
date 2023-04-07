fff93c33cc6fb5e33f36b34775d652d216ffcb5d
Vishal <vishalsingh6475@gmail.com>
2023-04-08 03:17:34 +1000

    Best time to buy stock II improved

--- 122 Best Time to Buy and Sell Stock II.cpp
+++ 122 Best Time to Buy and Sell Stock II.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+    
+        int maxP = 0;
+        
+        for (int i=1; i < prices.size(); i++) {
+            if (prices[i] > prices[i-1]) maxP += prices[i] - prices[i-1];
+        }
+
+        return maxP;
+        
+        /* method 2- slower
 
         int maxP = 0, last = prices[0], min = prices[0], max = -1;
         
@@ -18,6 +28,7 @@ public:
         if (max != -1) maxP += (max - min);
 
         return maxP;
+        */
         
     }
 };
